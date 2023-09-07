dab46a1c24cc5e4877a86665aed6d3c27d88698d
Sruthy-Raj-S <sruthyreena@gmail.com>
2023-09-07 13:15:24 +0530

    ecom

---
+++ ecommerceproject/search_app/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+from django.urls import path
+app_name='search_app'
+urlpatterns = [
+
+    path('', views.SearchResult, name='SearchResult'),
+
+
+
+
+
+
+
+
+]
